77edcdf9acecd6118b26b8cf1b45646ba441c6da
Coyote <Coyote@gmail.com>
2024-03-31 23:14:11 +0300

    Initial Commit

---
+++ HappyNewYear/HappyNewYearApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyNewYearApp.swift
+//  HappyNewYear
+//
+//  Created by SERGEI OSKIN on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct HappyNewYearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
